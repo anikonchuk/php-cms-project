eded34c009a6a776560bbc508678a3602df9b697
anikonchuk <anikonch@gmail.com>
2019-04-16 14:35:39 -0400

    Add table to categories.php and add dummy data

--- admin/categories.php
+++ admin/categories.php
@@ -26,12 +26,28 @@
                           <form action="index.html" method="post">
                             <div class="form-group">
                               <label for="cat-title">Add Category</label>
-                              <input class="form-control" id="cat-title" type="text" name="cat_title" value="">
+                              <input class="form-control" id="cat-title" type="text" name="cat_title">
                             </div>
                             <div class="form-group">
                               <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                             </div>
                           </form>
+                        </div>
+                        <div class="col-sm-6">
+                            <table class="table table-bordered table-hover">
+                              <thead>
+                                <tr>
+                                  <th>Id</th>
+                                  <th>Category Title</th>
+                                </tr>
+                              </thead>
+                              <tbody>
+                                <tr>
+                                  <td>Baseball Category</td>
+                                  <td>Basketball Category</td>
+                                </tr>
+                              </tbody>
+                            </table>
                         </div>
 
                     </div>

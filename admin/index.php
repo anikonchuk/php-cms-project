9f839e714c1c9d6bf8f30da7c891a8b46f5d371a
anikonchuk <anikonch@gmail.com>
2019-04-19 15:14:15 -0400

    Change admin/header, footer, navigation to admin_header, admin_footer, and admin_navigation

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,11 @@
-<?php include "includes/header.php"; ?>
+<?php include "includes/admin_header.php"; ?>
 
     <div id="wrapper">
 
 
         <!-- Navigation -->
 
-        <?php include "includes/navigation.php"; ?>
+        <?php include "includes/admin_navigation.php"; ?>
 
 
 
@@ -35,4 +35,4 @@
 
     </div>
     <!-- /#wrapper -->
-<?php include "includes/footer.php"; ?>
+<?php include "includes/admin_footer.php"; ?>
